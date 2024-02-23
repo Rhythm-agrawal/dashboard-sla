89286b09a872081c04584b1b57fa974155038e51
Rhythm Agrawal <ragrawa8@uncc.edu>
2024-02-23 16:13:00 -0500

    final commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Dashboard from "./components/Dashboard.vue";
 </script>
 
 <template>
-  <div id="app" class="container-fluid">
+  <div class="container-fluid">
     <Dashboard />
   </div>
 </template>
